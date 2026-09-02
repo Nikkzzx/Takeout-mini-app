74f249fe4fa8f86911264f1c7bcaa34113adb61a
nik <1274252679@qq.com>
2026-09-02 15:40:15 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -11,11 +12,17 @@ import java.util.List;
 public interface ShoppingCartMapper {
     //动态条件查询
     List<ShoppingCart> list(ShoppingCart shoppingCart);
+
     //根据id更新商品数量
     @Update("update shopping_cart set number = #{number} where id = #{id}")
     void updateNumberById(ShoppingCart shoppingCart);
+
     //插入购物车
     @Insert("insert into shopping_cart (user_id, dish_id, setmeal_id, name, dish_flavor, image, amount, number, create_time) " +
             "values (#{userId}, #{dishId}, #{setmealId}, #{name}, #{dishFlavor}, #{image}, #{amount}, #{number}, #{createTime})")
     void insert(ShoppingCart shoppingCart);
+
+    //删除指定用户的所有购物车数据
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void deleteByUserId(Long userId);
 }
